101f62bd3aeb545f4cde117404b0f2522f62dd85
Mari Koba <marieli530@gmail.com>
2014-02-08 15:38:47 -0800

    database access project

--- DatabaseAccess_Web/src/db_access.java
+++ DatabaseAccess_Web/src/db_access.java
@@ -5,13 +5,22 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.CreateSecurityGroupRequest;
+import com.amazonaws.services.ec2.model.CreateSecurityGroupResult;
 
 
 public class db_access {
 	public static void main(String [] args) throws IOException{
 		AWSCredentials credentials = 
-				new PropertiesCredentials(new File("../AwsCredentials.properties"));
+				new PropertiesCredentials(new File("src/AwsCredentials.properties"));
 		AmazonEC2Client amazonEC2Client = new AmazonEC2Client(credentials);
+		//Set to be same as Manager(??) thingie
+		amazonEC2Client.setEndpoint("ec2.us-west-2.amazonaws.com");
 		
+		//Creating security group
+		CreateSecurityGroupRequest createSecurityGroupRequest = new CreateSecurityGroupRequest();
+		createSecurityGroupRequest.withGroupName("CSE190Group").withDescription("190 Security Group");
+		CreateSecurityGroupResult createSecurityGroupResult = amazonEC2Client.createSecurityGroup(createSecurityGroupRequest);
+		System.out.println("done");
 	}
 }
