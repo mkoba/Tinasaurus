b04d555c5a857aa8e669d4bded104aeb282b0502
Mari Koba <marieli530@gmail.com>
2014-02-08 16:52:28 -0800

    connection not working

--- DatabaseAccess_Web/src/db_access.java
+++ DatabaseAccess_Web/src/db_access.java
@@ -1,5 +1,8 @@
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.PropertiesCredentials;
@@ -14,14 +17,36 @@ public class db_access {
 		//sup dudes
 		AWSCredentials credentials = 
 				new PropertiesCredentials(new File("src/AwsCredentials.properties"));
-		AmazonEC2Client amazonEC2Client = new AmazonEC2Client(credentials);
+		AmazonEC2 amazonEC2 = new AmazonEC2Client(credentials);
 		//Set to be same as Manager(??) thingie
-		amazonEC2Client.setEndpoint("ec2.us-west-2.amazonaws.com");
-		
+		amazonEC2.setEndpoint("ec2.us-west-2.amazonaws.com");
+		/*
 		//Creating security group
 		CreateSecurityGroupRequest createSecurityGroupRequest = new CreateSecurityGroupRequest();
 		createSecurityGroupRequest.withGroupName("CSE190Group").withDescription("190 Security Group");
-		CreateSecurityGroupResult createSecurityGroupResult = amazonEC2Client.createSecurityGroup(createSecurityGroupRequest);
+		CreateSecurityGroupResult createSecurityGroupResult = amazonEC2.createSecurityGroup(createSecurityGroupRequest);
 		System.out.println("done");
+		*/
+		String DNS = "http://ec2-54-200-149-123.us-west-2.compute.amazonaws.com/";
+		String myDBname = "test";
+		String MYSQLUSER = "frank.m.chao@jacobs.ucsd.edu";
+		String MYSQLPW = "";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Class.forName");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		Connection connection = null;
+		try{
+			connection = DriverManager.getConnection("jdbc:mysql://ec2-54-200-149-123.us-west-2.compute.amazonaws.com:3306/test?user=ec2-user&password=root");
+		} catch (SQLException e){
+			System.out.println("Driver Manager failed");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		System.out.println("HI :)");
 	}
 }
