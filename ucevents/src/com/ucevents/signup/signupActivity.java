154929c25d4d1f3cb316caddcf5206153a189801
Tina Szutu <tszutu@ucsd.edu>
2014-03-05 00:51:14 -0800

    T: update buttons for activites, add interest column for signup

--- ucevents/src/com/ucevents/signup/signupActivity.java
+++ ucevents/src/com/ucevents/signup/signupActivity.java
@@ -3,19 +3,53 @@ package com.ucevents.signup;
 import com.android.ucevents.R;
 import com.android.ucevents.R.layout;
 import com.google.analytics.tracking.android.EasyTracker;
+import com.ucevents.login.loginActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class signupActivity extends Activity{
-     Button signUp;
+     Button bsignUp;
+     Button bBack;
      
      public void onCreate(Bundle savedInstanceState) {
     	 super.onCreate(savedInstanceState);
     	 setContentView(R.layout.activity_signup);
+    	 addListenerOnButton();
     	
      }
+     
+     
+ 	private void addListenerOnButton() {
+		bsignUp = (Button) findViewById(R.id.signup);
+		bsignUp.setOnClickListener(new OnClickListener() {
+			 
+			//@Override
+			public void onClick(View arg0) {
+		
+				//Intent i= new Intent(loginActivity.this, com.ucevents.tab.Tabs.class);
+				Intent i= new Intent(signupActivity.this, com.ucevents.events.EventsActivity.class);
+				startActivity(i);
+				}
+			});
+		
+		bBack = (Button) findViewById(R.id.back);
+		bBack.setOnClickListener(new OnClickListener() {
+			 
+			//@Override
+			public void onClick(View arg0) {
+		
+				//Intent i= new Intent(loginActivity.this, com.ucevents.tab.Tabs.class);
+				Intent i= new Intent(signupActivity.this, com.android.ucevents.MainActivity.class);
+				startActivity(i);
+				}
+			});
+		
+	}
  	/**
  	 * An example Activity using Google Analytics and EasyTracker.
  	 */
