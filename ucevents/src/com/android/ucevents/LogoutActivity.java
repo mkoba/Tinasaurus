070e52cf6bbadb90a0b096a0b15b144d4501bb11
Judy Lin <judyclinn@gmail.com>
2014-03-12 14:17:21 -0700

    J: updated logout activity

--- ucevents/src/com/android/ucevents/LogoutActivity.java
+++ ucevents/src/com/android/ucevents/LogoutActivity.java
@@ -1,29 +1,56 @@
 package com.android.ucevents;
 
 import com.facebook.Session;
+import com.ucevents.events.CategoryEventsActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class LogoutActivity extends Activity {
+	private Button logout;
+	private Button cancel;
+	
 	@Override 
 	public void onCreate(Bundle savedIntanceState) {
 		super.onCreate(savedIntanceState);
+		setContentView(R.layout.logout_popup);
 		
-		Session session = Session.getActiveSession();
-		if (session != null) {
-			System.out.println("got a session");
-	        if (!session.isClosed()) {
-	            session.closeAndClearTokenInformation();
-	            Intent i = new Intent(LogoutActivity.this, MainActivity.class);
-	            startActivity(i);
-	            //clear your preferences if saved
-	        }
-	    } else {
-
-	    	System.out.println("cannot find session");
-
-	    }
+		logout = (Button)findViewById(R.id.logout);
+		cancel = (Button)findViewById(R.id.cancel);
+		
+		logout.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View arg0){
+				Session session = Session.getActiveSession();
+				if (session != null) {
+					System.out.println("got a session");
+			        if (!session.isClosed()) {
+			            session.closeAndClearTokenInformation();
+			            Intent i = new Intent(LogoutActivity.this, MainActivity.class);
+			            startActivity(i);
+			            //clear your preferences if saved
+			        }
+			    } else {
+	
+			    	System.out.println("cannot find session");
+	
+			    }
+			}
+			
+		});
+		
+		
+		cancel.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View arg0){
+				Intent i = new Intent(LogoutActivity.this, com.ucevents.events.EventsActivity.class );
+				startActivity(i);
+			}
+			
+		});	
 	}
 }
--- ucevents/src/com/ucevents/menu/MenuActivity.java
+++ ucevents/src/com/ucevents/menu/MenuActivity.java
@@ -2,26 +2,31 @@ package com.ucevents.menu;
 
 import com.android.ucevents.R;
 
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MenuActivity extends Activity{
+    
 	Button ballEvents;
 	Button bbyCategory;
 	Button bUserInterest;
 	Button logout;
-/*	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.frag_events);
-    }*/
 	
 	
 	@Override
@@ -82,4 +87,5 @@ public class MenuActivity extends Activity{
 		Intent intent = new Intent(MenuActivity.this, com.android.ucevents.LogoutActivity.class);
 		startActivity(intent);
 	}
+	
 }
