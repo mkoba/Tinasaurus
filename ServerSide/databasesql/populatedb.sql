392b66c15e71c8fd1f882e984411f744c7bfeec3
Mari Koba <marieli530@gmail.com>
2014-03-16 16:21:58 -0700

    populate db

---
+++ ServerSide/databasesql/populatedb.sql
@@ -0,0 +1,124 @@
+/*
+	marieli530@gmail.com
+	snapdragon514@yahoo.com
+	leogungaurdian@yahoo.com
+	lcam@ucsd.edu
+	erikgallegos_886@hotmail.com
+	mynamelsfrank@yahoo.com*/
+
+/*
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('erikgallegos_886@hotmail.com_Sun God Festival', 'Rimac Field', 17, 30, 5, 17, 2014, 'Join us at the festival of gods!', 'erikgallegos_886@hotmail.com', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('erikgallegos_886@hotmail.com_Game Night!', 'International Center', 18, 00, 3, 25, 2014, 'GAME ON!!!', 'erikgallegos_886@hotmail.com', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('erikgallegos_886@hotmail.com_Free Desserts!', 'MOM', 9, 30, 3, 20, 2014, 'Stressed is Desserts Backwards!', 'erikgallegos_886@hotmail.com', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('erikgallegos_886@hotmail.com_Pancake Night', 'Price Center Ballroom East', 18, 30, 3, 22, 2014, 'Free Pancakes!', 'erikgallegos_886@hotmail.com', true);
+
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('snapdragon514@yahoo.com_CSE Interview Practice', 'CSE basement', 18, 30, 3, 22, 2014, 'Land that job or internship! Come to the CSE basement for more interview practice', 'snapdragon514@yahoo.com', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('snapdragon514@yahoo.com_CSE 190 Presentations', 'CSE basement', 14, 30, 3, 19, 2014, 'Come check out all the wonderful 190 apps of Winter Quarter', 'snapdragon514@yahoo.com', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('snapdragon514@yahoo.com_HKN Study Inn', 'EBU1', 14, 30, 3, 21, 2014, 'Need a place to study? Join HKN at the study lounge. Food and drinks provided', 'snapdragon514@yahoo.com', true);
+
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('lcam@ucsd.edu_Puppy Love', 'Library Walk', 11, 30, 3, 21, 2014, 'Need some puppy love? Come down to library walk and adopt a puppy today!', 'lcam@ucsd.edu', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('lcam@ucsd.edu_Create: Tea Cups', 'Muir Quad', 11, 30, 3, 28, 2014, 'Art club is making teacups for the spring. Come on over and paint your very own tea cup', 'lcam@ucsd.edu', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('lcam@ucsd.edu_Google Info Session', 'Career Center', 19, 30, 3, 21, 2014, 'Meet recruiters and learn more about Google!', 'lcam@ucsd.edu', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('mynamelsfrank@yahoo.com_Microsoft Info Session', 'Career Center', 19, 30, 3, 25, 2014, 'Meet recruiters and learn more about Microsoft!', 'mynamelsfrank@yahoo.com', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('mynamelsfrank@yahoo.com_Intuit Session', 'Career Center', 11, 30, 4, 23, 2014, 'Meet recruiters and learn more about Intuit!', 'mynamelsfrank@yahoo.com', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('mynamelsfrank@yahoo.com_Open Mic Night', 'The Loft', 18, 00, 4, 12, 2014, 'Come over for a night of hidden talent', 'mynamelsfrank@yahoo.com', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('mynamelsfrank@yahoo.com_Color Run', 'Rimac Field', 18, 00, 7, 12, 2014, 'Run in the color!', 'mynamelsfrank@yahoo.com', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('mynamelsfrank@yahoo.com_FusionX Dance Competition', 'Mandeville', 18, 00, 10, 17, 2014, 'Dance competition!', 'mynamelsfrank@yahoo.com', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('mynamelsfrank@yahoo.com_Taco Tuesday!', 'Library Walk', 9, 00, 6, 10, 2014, 'Buy all the tacos!', 'mynamelsfrank@yahoo.com', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('lcam@ucsd.edu_Porter Pub Night', 'Library Walk', 19, 00, 5, 10, 2014, 'Check out my bartending skills', 'lcam@ucsd.edu', true);
+
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('lcam@ucsd.edu_Casino Night', 'Porter Pub', 17, 00, 4, 24, 2014, 'Casino themed night!', 'lcam@ucsd.edu', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('lcam@ucsd.edu_MuirStock', 'Muir Quad', 18, 00, 4, 22, 2014, 'Join with all the music!', 'lcam@ucsd.edu', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('lcam@ucsd.edu_Rock Climbing', 'Gym', 15, 00, 3, 29, 2014, 'Free Rocking climbing week one!', 'lcam@ucsd.edu', true);
+INSERT INTO events (name, location, hour, min, month, date, year, description, host, public) VALUES ('lcam@ucsd.edu_Weight Competition', 'Gym', 16, 00, 4, 4, 2014, 'How much can you bench?', 'lcam@ucsd.edu', true);
+
+
+INSERT INTO event_category (event, interest) VALUES ('erikgallegos_886@hotmail.com_Sun God Festival', 'social');
+INSERT INTO event_category (event, interest) VALUES ('erikgallegos_886@hotmail.com_Game Night!', 'social');
+INSERT INTO event_category (event, interest) VALUES ('erikgallegos_886@hotmail.com_Free Desserts!', 'food');
+INSERT INTO event_category (event, interest) VALUES ('erikgallegos_886@hotmail.com_Pancake Night', 'food');
+INSERT INTO event_category (event, interest) VALUES ('snapdragon514@yahoo.com_CSE Interview Practice', 'study');
+INSERT INTO event_category (event, interest) VALUES ('snapdragon514@yahoo.com_HKN Study Inn', 'study');
+INSERT INTO event_category (event, interest) VALUES ('snapdragon514@yahoo.com_HKN Study Inn', 'organization');
+INSERT INTO event_category (event, interest) VALUES ('lcam@ucsd.edu_Puppy Love', 'social');
+INSERT INTO event_category (event, interest) VALUES ('lcam@ucsd.edu_Create: Tea Cups', 'social');
+INSERT INTO event_category (event, interest) VALUES ('lcam@ucsd.edu_Create: Tea Cups', 'organization');
+INSERT INTO event_category (event, interest) VALUES ('lcam@ucsd.edu_Google Info Session', 'career');
+INSERT INTO event_category (event, interest) VALUES ('mynamelsfrank@yahoo.com_Intuit Session', 'career');
+INSERT INTO event_category (event, interest) VALUES ('mynamelsfrank@yahoo.com_Microsoft Info Session', 'career');
+INSERT INTO event_category (event, interest) VALUES ('mynamelsfrank@yahoo.com_Open Mic Night', 'social');
+INSERT INTO event_category (event, interest) VALUES ('mynamelsfrank@yahoo.com_Open Mic Night', 'other');
+INSERT INTO event_category (event, interest) VALUES ('mynamelsfrank@yahoo.com_Color Run', 'sports');
+INSERT INTO event_category (event, interest) VALUES ('mynamelsfrank@yahoo.com_FusionX Dance Competition', 'social');
+INSERT INTO event_category (event, interest) VALUES ('mynamelsfrank@yahoo.com_FusionX Dance Competition', 'organization');
+INSERT INTO event_category (event, interest) VALUES ('mynamelsfrank@yahoo.com_FusionX Dance Competition', 'other');
+INSERT INTO event_category (event, interest) VALUES ('mynamelsfrank@yahoo.com_FusionX Dance Competition', 'sports');
+INSERT INTO event_category (event, interest) VALUES ('mynamelsfrank@yahoo.com_Taco Tuesday!', 'food');
+INSERT INTO event_category (event, interest) VALUES ('lcam@ucsd.edu_Porter Pub Night', 'food');
+INSERT INTO event_category (event, interest) VALUES ('lcam@ucsd.edu_Casino Night', 'food');
+INSERT INTO event_category (event, interest) VALUES ('lcam@ucsd.edu_MuirStock', 'social');
+INSERT INTO event_category (event, interest) VALUES ('lcam@ucsd.edu_Rock Climbing', 'sports');
+INSERT INTO event_category (event, interest) VALUES ('lcam@ucsd.edu_Weight Competition', 'sports');
+INSERT INTO event_category (event, interest) VALUES ('lcam@ucsd.edu_Weight Competition', 'other');
+
+INSERT INTO attendees (event, attendee) VALUES ('erikgallegos_886@hotmail.com_Sun God Festival', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('erikgallegos_886@hotmail.com_Game Night!', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('erikgallegos_886@hotmail.com_Free Desserts!', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('erikgallegos_886@hotmail.com_Pancake Night', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('snapdragon514@yahoo.com_CSE Interview Practice', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('snapdragon514@yahoo.com_HKN Study Inn', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_Puppy Love', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_Create: Tea Cups', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_Google Info Session', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('mynamelsfrank@yahoo.com_Intuit Session', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('mynamelsfrank@yahoo.com_Microsoft Info Session', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('mynamelsfrank@yahoo.com_Open Mic Night', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('mynamelsfrank@yahoo.com_Color Run', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('mynamelsfrank@yahoo.com_FusionX Dance Competition', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('mynamelsfrank@yahoo.com_Taco Tuesday!', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_Porter Pub Night', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_MuirStock', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_Rock Climbing', 'erikgallegos_886@hotmail.com');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_Weight Competition', 'erikgallegos_886@hotmail.com');
+
+INSERT INTO attendees (event, attendee) VALUES ('erikgallegos_886@hotmail.com_Sun God Festival', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('erikgallegos_886@hotmail.com_Game Night!', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('erikgallegos_886@hotmail.com_Free Desserts!', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('erikgallegos_886@hotmail.com_Pancake Night', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('snapdragon514@yahoo.com_CSE Interview Practice', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('snapdragon514@yahoo.com_HKN Study Inn', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_Puppy Love', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_Create: Tea Cups', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_Google Info Session', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('mynamelsfrank@yahoo.com_Intuit Session', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('mynamelsfrank@yahoo.com_Microsoft Info Session', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('mynamelsfrank@yahoo.com_Open Mic Night', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('mynamelsfrank@yahoo.com_Color Run', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('mynamelsfrank@yahoo.com_FusionX Dance Competition', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('mynamelsfrank@yahoo.com_Taco Tuesday!', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_Porter Pub Night', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_MuirStock', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_Rock Climbing', 'lcam@ucsd.edu');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_Weight Competition', 'lcam@ucsd.edu');
+
+INSERT INTO attendees (event, attendee) VALUES ('erikgallegos_886@hotmail.com_Sun God Festival', 'leogungaurdian@yahoo.com');
+INSERT INTO attendees (event, attendee) VALUES ('erikgallegos_886@hotmail.com_Free Desserts!', 'leogungaurdian@yahoo.com');
+INSERT INTO attendees (event, attendee) VALUES ('snapdragon514@yahoo.com_CSE Interview Practice', 'leogungaurdian@yahoo.com');
+INSERT INTO attendees (event, attendee) VALUES ('snapdragon514@yahoo.com_HKN Study Inn', 'leogungaurdian@yahoo.com');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_Puppy Love', 'leogungaurdian@yahoo.com');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_Create: Tea Cups', 'leogungaurdian@yahoo.com');
+INSERT INTO attendees (event, attendee) VALUES ('mynamelsfrank@yahoo.com_Microsoft Info Session', 'leogungaurdian@yahoo.com');
+INSERT INTO attendees (event, attendee) VALUES ('mynamelsfrank@yahoo.com_Open Mic Night', 'leogungaurdian@yahoo.com');
+INSERT INTO attendees (event, attendee) VALUES ('mynamelsfrank@yahoo.com_FusionX Dance Competition', 'leogungaurdian@yahoo.com');
+INSERT INTO attendees (event, attendee) VALUES ('mynamelsfrank@yahoo.com_Taco Tuesday!', 'leogungaurdian@yahoo.com');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_MuirStock', 'leogungaurdian@yahoo.com');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_Rock Climbing', 'leogungaurdian@yahoo.com');
+INSERT INTO attendees (event, attendee) VALUES ('lcam@ucsd.edu_Weight Competition', 'leogungaurdian@yahoo.com');*/
+
+
+SELECT * FROM user_information;
+SELECT * FROM interests;
+SELECT * FROM user_interests;
+SELECT * FROM events;
+SELECT * FROM event_category;
+SELECT * FROM attendees;
